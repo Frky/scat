c3a6a7d323ce80d52896d771c56489ad7cf1ad30
aumgn <aumgn@free.fr>
2016-04-17 10:58:07 +0200

    Add HollowStack
    Fix (once and for all ?) the problem of using a call stack with a fixed
    capacity.
    See: test/call_stack_overflow.c for an illustration of the problem
    See: src/pintool/utils/hollow_stack.h for the implementation of the solution
    
    Apparently also fix the problem of some standard functions (memchr, strcmp, strlen, etc...) reported with too many arguments.
    See: https://gist.github.com/aumgn/74067aad321841e992fe54d4d33a6097/revisions
    
    Also add an "utils" directory in "src/pintool" for files other than pintool main source which get copied inside $(PINROOT)/source/tools/pinalloc for the compilation of the tools.

---
+++ src/pintool/utils/hollow_stack.h
@@ -0,0 +1,162 @@
+#ifndef HOLLOW_STACK_H_
+#define HOLLOW_STACK_H_
+
+#include <exception>
+
+class EmptyStackException : public std::exception {};
+
+class IgnoredElementException : public std::exception {};
+
+/**
+ * HollowStack
+ *
+ * Acts as a regular stack with the methods:
+ *    `push`, `top` and `pop`
+ * but also adds :
+ *    - Random access to elements for a given height.
+ *    - Hollowness: The stack has a fixed constant capacity,
+ *      thus providing fast and constant operations.
+ *      When the capacity is reached, elements at the middle of
+ *      the stack are forgotten.
+ *      For example with a capacity of 100, if more than 100 elements
+ *      are inserted, the stack will keep the 50 bottom-most elements
+ *      and the 50 top-most elements.
+ */
+template <unsigned int CAPACITY, typename T>
+class HollowStack {
+
+public:
+
+    HollowStack():
+            bottom_(),
+            top_(),
+            height_(-1),
+            middleSize_(0),
+            topFirst_(0),
+            topSize_(0) {}
+
+    /**
+     * Check if the stack is empty
+     */
+    inline bool is_empty() {
+        return height_ < 0;
+    }
+
+    /**
+     * Returns the height of this stack.
+     * Also the height of the top element.
+     */
+    inline int height() {
+        if (is_empty()) throw new EmptyStackException;
+        return height_;
+    }
+
+    /**
+     * Tells if the top element has been forgotten
+     */
+    inline bool is_top_forgotten() {
+        return is_forgotten(height_);
+    }
+
+    /**
+     * Tells if the element at the given `height` has
+     * been forgotten
+     */
+    bool is_forgotten(int height) {
+        if (is_empty()) throw new EmptyStackException;
+
+        int c = height - BOTTOM_CAPACITY;
+        return c >= 0 && c < middleSize_;
+    }
+
+    /**
+     * Pushes the given `element` on top of this stack.
+     * This may lead to some elements pushed earlier
+     * being forgotten if this stack's capacity has
+     * been reached
+     */
+    void push(T element) {
+        height_++;
+        if (height_ < BOTTOM_CAPACITY) {
+            bottom_[height_] = element;
+        }
+        else if (topSize_ < TOP_CAPACITY) {
+            top_[topSize_] = element;
+            topSize_++;
+        }
+        else {
+            top_[topFirst_] = element;
+            middleSize_++;
+            topFirst_ = modulo(topFirst_ + 1, TOP_CAPACITY);
+        }
+    }
+
+    /**
+     * Returns the element on top of this stack
+     */
+    inline T top() {
+        return peek(height_);
+    }
+
+    /**
+     * Returns the element at the given `height`
+     * in this stack
+     */
+    T peek(int height) {
+        if (is_empty()) throw new EmptyStackException;
+
+        if (height < BOTTOM_CAPACITY) {
+            return bottom_[height];
+        }
+        else if (height < BOTTOM_CAPACITY + middleSize_) {
+            throw new IgnoredElementException;
+        }
+        else {
+            int topIndex = height - BOTTOM_CAPACITY - middleSize_;
+            return top_[modulo(topFirst_ + topIndex, TOP_CAPACITY)];
+        }
+    }
+
+    /**
+     * Discards the top element of this stack
+     */
+    void pop() {
+        if (is_empty()) throw new EmptyStackException;
+
+        if (height_ < BOTTOM_CAPACITY) {
+            height_--;
+        }
+        else if (height_ < BOTTOM_CAPACITY + middleSize_) {
+            height_--;
+            if (middleSize_ > 0) {
+                middleSize_--;
+            }
+        }
+        else {
+            height_--;
+            topSize_--;
+        }
+    }
+
+private:
+
+    static const int BOTTOM_CAPACITY = (CAPACITY >> 1) + (CAPACITY & 1);
+    static const int TOP_CAPACITY = (CAPACITY >> 1);
+
+    T bottom_[BOTTOM_CAPACITY];
+    T top_[TOP_CAPACITY];
+
+    int height_;
+    int middleSize_;
+    int topFirst_;
+    int topSize_;
+
+    static inline int modulo(int dividend, int divisor) {
+        int modulo = dividend % divisor;
+        return modulo < 0
+               ? modulo + divisor
+               : modulo;
+    }
+};
+
+#endif
