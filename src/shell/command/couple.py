d8ad062cfad1a2edf378d2a7bc49adaac54d9254
Nicolas BORDES <nicolas.bordes2@free.fr>
2017-05-30 12:17:19 +0200

    Enhance cli experience

--- src/shell/command/couple.py
+++ src/shell/command/couple.py
@@ -18,5 +18,15 @@ class CoupleCmd(ICommand):
         if "couple" not in self.__pintools.keys():
             self.stderr("you must run memblock inference first")
             return
-        logfile = self.__pintools["couple"].get_logfile(s, prev=False)
-        Couple(logfile, s, self.stdout).run()
+        if len(s) == 0:
+            self.stderr("You must give at least one argument (pgm)")
+            return
+
+        s = s.split()
+
+        try:
+            logfile = self.__pintools["couple"].get_logfile(s[0], prev=False)
+        except IOError:
+            self.stderr("Logs for binary \"{}\" not found".format(s[0]))
+            return
+        Couple(logfile, s[0], self.stdout).run()
