1d668c13ced85ac2ecb88aa833f4d6c491ed979e
Frky <degoerdf@imag.fr>
2017-06-05 14:34:20 +0200

    Refacto test command

--- src/shell/command/test.py
+++ src/shell/command/test.py
@@ -21,45 +21,64 @@ class TestCmd(ICommand):
     def run(self, s, *args, **kwargs):
         split = s.split(" ")
 
-        if len(split) < 1:
-            return 
-
+        # Watch for a particular test description file
+        # if not specified, the one provided by the configuration file is used
         if "-t" in split:
             self.__conf_path = split.pop(split.index("-t") + 1)
             split.pop(split.index("-t"))
-        command = split[0]
 
-        # Test accuracy
-        if command == "accuracy":
-            if len(split) > 1:
-                subcommand = split[1]
-            else:
-                subcommand = None
-            TestAccuracy(self.__conf_path, self.__pintools["arity"], self.__pintools["type"], self.__logdir, resdir=self.__resdir).run(subcommand)
-        elif command == "param":
-            if len(split) < 2 or split[1] not in ["arity", "type"]: 
-                self.stderr("Unknown or missing analysis (arity, type) -- aborting")
+        if len(split) < 1:
+            self.help()
+            return 
+
+        analysis = split[0]
+        
+        if analysis == "arity" or analysis == "type":
+            if len(split) < 2:
+                self.help()
                 return 
-            analysis = split[1]
-            if len(split) < 3 or split[2] not in self.__param[analysis].keys():
-                self.stderr("Unknown or missing parameter -- aborting")
-                return
-            param = split[2]
-            vals = list()
-            v = self.__param[analysis][param]["min"]
-            step = self.__param[analysis][param]["step"]
-            while v <= self.__param[analysis][param]["max"]: 
-                vals.append(round(v, 2))
-                v += step
-            params = dict()
-            params[param] = vals
-            for p, v in self.__param[analysis][param].items():
-                if p not in ["min", "max", "step"]:
-                    params[p] = v
-            if analysis == "arity":
-                prev = None
+            param = split[1]
+            if param == "accuracy":
+                TestAccuracy(
+                                self.__conf_path, 
+                                self.__pintools["arity"], 
+                                self.__pintools["type"], 
+                                self.__logdir, 
+                                resdir=self.__resdir).run()
             else:
-                prev = self.__pintools["arity"]
-            test = TestParameter(self.__conf_path, self.__pintools[analysis], resdir=self.__resdir, prev_pintool=prev)
-            test.run(params)
+                if param not in self.__param[analysis].keys():
+                    self.stderr("unknown parameter: {} -- aborting".format(param))
+                vals = list()
+                v = self.__param[analysis][param]["min"]
+                step = self.__param[analysis][param]["step"]
+                while v <= self.__param[analysis][param]["max"]: 
+                    vals.append(round(v, 2))
+                    v += step
+                params = dict()
+                params[param] = vals
+                for p, v in self.__param[analysis][param].items():
+                    if p not in ["min", "max", "step"]:
+                        params[p] = v
+                if analysis == "arity":
+                    prev = None
+                else:
+                    prev = self.__pintools["arity"]
+                test = TestParameter(
+                                        self.__conf_path, self.__pintools[analysis], 
+                                        resdir=self.__resdir, 
+                                        prev_pintool=prev
+                                    )
+                test.run(params)
+        elif analysis == "couple":
+            if len(split) < 2:
+                self.help()
+                return 
+            param = split[1]
+            if param == "general":
+                pass
+            else: 
+                self.stderr("unknown parameter: {} -- aborting".format(param))
+                raise Exception("not implemented yet")
+        else:
+            self.stderr("unknown analysis: {} -- aborting".format(analysis))
         return
